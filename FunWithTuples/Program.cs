e59a046030254000ef81f1cd52a8af2c972163b0
unknown <Dror@GrundigTV.(none)>
2012-06-01 12:33:41 +0300

    Fun with Tuples

---
+++ FunWithTuples/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FunWithTuples
+{
+    class Program
+    {
+        public static List<string> GenerateStringList(int number)
+        {
+            List<string> retval = new List<string>();
+            for (int i = 0; i < number; i++)
+                retval.Add("Item " + i.ToString());
+
+            return retval;
+        }
+
+
+        public static Tuple<int, List<string>> GetPagedItems(int from, int len)
+        {
+            var list = GenerateStringList(1000);
+
+            var listcount  = list.Count();
+            var subset = list.OrderBy(i => i).Skip(from).Take(len).ToList();
+
+            return new Tuple<int, List<string>>(listcount, subset);
+        }
+
+        static void Main(string[] args)
+        {
+            var pageditems = GetPagedItems(4, 5);
+            Console.WriteLine("Items: " + pageditems.Item1);
+            foreach (var item in pageditems.Item2)
+                Console.WriteLine(item);
+
+
+
+
+        }
+    }
+}
