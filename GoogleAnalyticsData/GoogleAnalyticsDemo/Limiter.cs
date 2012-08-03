bb9d57a54b362d8dd411b9eb7bd2a5a6dcec11e7
unknown <Dror@GrundigTV.(none)>
2012-08-03 07:55:31 +0300

    Google Analytics demo

---
+++ GoogleAnalyticsData/GoogleAnalyticsDemo/Limiter.cs
@@ -0,0 +1,141 @@
+﻿/*
+Limiter - Limits calls per interval and concurrency
+Copyright (c) 2012 Dror Gluska
+	
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public License
+(LGPL) as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+The terms of redistributing and/or modifying this software also
+include exceptions to the LGPL that facilitate static linking.
+ 	
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+ 	
+You should have received a copy of the GNU Lesser General Public License
+along with this library; if not, write to Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+
+This class can be easily changed to be a generic class but will have to 
+drop the Stream inheritance, if you require multiple writes/reads/peeks, feel free.
+  
+  
+Change log:
+2012-08-03 - Initial version
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace GoogleAnalyticsDemo
+{
+    /// <summary>
+    /// Class which allowes you control over how many calls per interval and how many concurrent calls
+    /// <para>Works like a regular lock.</para>
+    /// </summary>
+    public class Limiter
+    {
+        /// <summary>
+        /// Holds the latest datetime in which the requests per interval is valid
+        /// </summary>
+        private DateTime _last;
+
+        /// <summary>
+        /// Interval in which we're limiting the calls.
+        /// </summary>
+        private TimeSpan _interval;
+
+        /// <summary>
+        /// current calls per interval, this is zeroes each interval.
+        /// </summary>
+        private volatile int _callsperinterval;
+
+        /// <summary>
+        /// Maximum calls per interval
+        /// </summary>
+        private volatile int _maxcallsperinterval;
+
+
+        /// <summary>
+        /// Concurrent requests counter
+        /// </summary>
+        private volatile int _concurrentcalls;
+
+        /// <summary>
+        /// Maximum concurrent calls counter
+        /// </summary>
+        private volatile int _maxconcurrentcalls;
+
+        /// <summary>
+        /// Lock for calls counters.
+        /// </summary>
+        private SpinLock _callslock = new SpinLock();
+
+        /// <summary>
+        /// Initializes a new instance of the Limiter class
+        /// </summary>
+        /// <param name="interval">time span for maxRequestsPerInterval</param>
+        /// <param name="maxCallsPerInterval">maximum calls per interval</param>
+        /// <param name="maxConcurrent">maximum concurrent calls</param>
+        public Limiter(TimeSpan interval, int maxCallsPerInterval, int maxConcurrent)
+        {
+            _interval = interval;
+            _last = DateTime.Now + _interval;
+
+            _callsperinterval = 0;
+            _maxcallsperinterval = maxCallsPerInterval;
+            _concurrentcalls = 0;
+            _maxconcurrentcalls = maxConcurrent;
+        }
+
+
+        /// <summary>
+        /// Acquire a call, blocks if not available until available.
+        /// </summary>
+        public void Enter()
+        {
+            
+            while (true)
+            {
+                var locktaken = false;
+                _callslock.Enter(ref locktaken);
+
+                if (_last < DateTime.Now)
+                {
+                    _last = DateTime.Now + _interval;
+                    _callsperinterval = 0;
+                }
+
+                //check interval
+                //check concurrent
+                if ((_concurrentcalls < _maxconcurrentcalls) && (_callsperinterval < _maxcallsperinterval))
+                {
+                    Interlocked.Increment(ref _concurrentcalls);
+                    Interlocked.Increment(ref _callsperinterval);
+                    _callslock.Exit();
+                    break;
+                }
+                _callslock.Exit();
+                Thread.Sleep(1);
+            }
+        }
+
+        /// <summary>
+        /// Releases a call
+        /// </summary>
+        public void Exit()
+        {
+            //reduce concurrent
+            var locktaken = false;
+            _callslock.Enter(ref locktaken);
+            Interlocked.Decrement(ref _concurrentcalls);
+            _callslock.Exit();
+        }
+    }
+}
---
+++ GoogleAnalyticsData/GoogleAnalyticsDemo/Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Google.GData.Analytics;
+using PublicDomain;
+
+namespace GoogleAnalyticsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //test limiter
+            //TestLimiter();
+
+            //initialize a new limiter with 10 concurrent and 10 requests per second.
+            Limiter limiter = new Limiter(TimeSpan.FromSeconds(1), 10, 10);
+
+            string username = "username";
+            string password = "password";
+
+            //first, initialize a service, its the query provider.
+            var service = new Google.GData.Analytics.AnalyticsService("AnalyticsReader");
+
+            //you can also use service.setUserCredentials(username,password)
+            service.Credentials = new Google.GData.Client.GDataCredentials(username, password);
+
+            //first, query for all accounts
+            var accountquery = new Google.GData.Analytics.AccountQuery();
+
+            accountquery.NumberToRetrieve = 10000; //Maximum
+
+            limiter.Enter();
+            var accountresult = service.Query(accountquery);
+            limiter.Exit();
+
+            //for each account, retrieve records
+            foreach (AccountEntry account in accountresult.Entries)
+            {
+                //get the timezone for the account, as all data is saved based on that timezone.
+                Property timezone = account.Properties.Where(i => i.Name == "ga:timezone").FirstOrDefault();
+
+
+                Console.WriteLine(
+                  "\nProfile Title     = " + account.Title.Text +
+                  "\nProfile ID        = " + account.ProfileId.Value + 
+                  "\nTimeZone       = " + timezone.Value);
+
+                TzTimeZone tzinfo = null;
+
+                if (timezone != null)
+                    tzinfo = TzTimeZone.GetTimeZone(timezone.Value);
+
+                //retrieve analytics data
+                DataQuery query = new DataQuery();
+                query.Ids = account.ProfileId.Value;
+
+                //Data
+
+                //dimensions are the "group by"
+                query.Dimensions = "ga:date,ga:hour,ga:hostname,ga:country,ga:keyword,ga:source,ga:referralPath";
+
+                //metrics are the values
+                query.Metrics = "ga:bounces,ga:newVisits,ga:pageviews,ga:timeOnSite,ga:visits";
+                
+                //start from yesterday
+                query.GAStartDate = DateTime.Now.AddDays(-1).ToString("yyyy-MM-dd");
+
+                //until today
+                query.GAEndDate = DateTime.Now.ToString("yyyy-MM-dd");
+                query.StartIndex = 1;
+
+                //Maximum allowed by Quota.
+                query.NumberToRetrieve = 10000;
+
+
+                limiter.Enter();
+                DataFeed dataFeedVisits = service.Query(query);
+                limiter.Exit();
+
+                //process each record returned
+                foreach (DataEntry dentry in dataFeedVisits.Entries)
+                {
+                    var line = new Dictionary<string, string>();
+
+                    //insert into directory for each processing
+                    for (int i = 0; i < dentry.Metrics.Count; i++)
+                        line[dentry.Metrics[i].Name] = dentry.Metrics[i].Value;
+
+                    for (int i = 0; i < dentry.Dimensions.Count; i++)
+                        line[dentry.Dimensions[i].Name] = dentry.Dimensions[i].Value;
+
+                    //get the UTC datetime from the ga:date + ga:hour + profile timezone combination
+                    DateTime date = DateTime.ParseExact(line["ga:date"], "yyyyMMdd", System.Globalization.CultureInfo.CurrentCulture);
+                    int hour = Convert.ToInt32(line["ga:hour"]);
+                    date = date.AddHours(hour);
+
+                    line["dateutc"] = ConvertToUtc(date, tzinfo).ToString();
+
+
+                    //dump to console all data found
+                    Console.Write("UTC: {0}, ", line["dateutc"]);
+
+                    for (int i = 0; i < dentry.Dimensions.Count; i++)
+                        Console.Write("{0}: {1}, ", dentry.Dimensions[i].Name, dentry.Dimensions[i].Value);
+
+                    for (int i = 0; i < dentry.Metrics.Count; i++)
+                        Console.Write("{0}: {1}, ", dentry.Metrics[i].Name, dentry.Metrics[i].Value);
+
+                    Console.WriteLine();
+
+                }
+
+            }
+
+        }
+
+        /// <summary>
+        /// Converts a datetime from fromTimeZone to UTC
+        /// </summary>
+        private static DateTime ConvertToUtc(DateTime datetime, TzTimeZone fromTimeZone)
+        {
+            return fromTimeZone.ToUniversalTime(datetime);
+        }
+
+
+        #region test limiter
+        private static void TestLimiter()
+        {
+            int concurrent = 0;
+            DateTime current = DateTime.Now;
+
+            Limiter limiter = new Limiter(TimeSpan.FromSeconds(1), 10, 10);
+
+
+            Task t = new Task(() =>
+                {
+                    for (var i = 0; i < 1000; i++)
+                    {
+                        if (current.Second != DateTime.Now.Second)
+                        {
+                            current = DateTime.Now;
+                            Console.WriteLine(concurrent);
+                            concurrent = 0;
+                        }
+                        Thread.Sleep(100);
+                    }
+                });
+            t.Start();
+
+            List<Task> tasks = new List<Task>();
+            for (var x= 0; x < 200; x++)
+                tasks.Add(
+             new Task(() =>
+                {
+                    for (var i = 0; i < 200; i++)
+                    {
+                        limiter.Enter();
+                        Interlocked.Increment(ref concurrent);
+                        limiter.Exit();
+                    }
+                })
+               );
+
+            foreach (var c in tasks)
+                c.Start();
+
+            foreach (var c in tasks)
+                c.Wait();
+
+            t.Wait();
+
+        }
+        #endregion
+
+    }
+}
